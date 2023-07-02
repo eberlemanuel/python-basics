666147e500e2aa2cc9a32ff934ecc4439ae57c98
Manuel Eberle <>
2023-07-02 14:18:36 +0200

    create list training

---
+++ 02-listen/list.py
@@ -0,0 +1,24 @@
+# Statt
+student1 = "Hans"
+student2 = "Max"
+student3 = "Rosa"
+
+# Listen verwenden
+students = ["Hans", "Max", "Rosa"]
+
+# Liste ausgeben
+print(students)
+
+# Element hinzufügen
+students.append("Franz")
+print(students)
+
+# Länge der Liste
+print(len(students))
+
+# Student Nr. 2 ausgeben -> Index beginnt immer bei 0
+print(students[1])
+
+# Element löschen
+students.remove("Max")
+print(students)
---
+++ 02-listen/quiz.py
@@ -0,0 +1,7 @@
+# Definiere eine Liste mit Zahlen von 1 - 3
+
+# Schreibe den Code um Donald auszugeben
+firstnames = ["Ivanka", "Donald", "Ivana", "Barron"]
+
+# Erweitere die Liste um das Element "Test"
+list = ["Hallo"]
