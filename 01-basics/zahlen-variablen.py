bafe63a26662fa16d418e0c5999de5cf4784d064
Manuel Eberle <manueleberle9@gmail.com>
2023-07-02 13:52:37 +0200

    Create zahlen-variablen.py

---
+++ 01-basics/zahlen-variablen.py
@@ -0,0 +1,20 @@
+# Ausgeben einer Zahl
+print(5)
+
+# Abarbeitung Zeile für Zeile
+print(3)
+print(10)
+
+# Zahlen Operationen 
+print(5 + 6)
+print(5 - 6)
+print(5 * 6)
+print(5 / 6)
+
+# Klammerung
+print((5 + 3) * 2)
+
+# Speichern von Werten und Wiederverwendbarkeit
+ergebnis = 5 + 6
+print(ergebnis)
+print(ergebnis + 1)
