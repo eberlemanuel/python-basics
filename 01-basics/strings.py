fe6d0711522f1196e7ba127fa00f56bc2b19a682
Manuel Eberle <manueleberle9@gmail.com>
2023-07-02 14:04:21 +0200

    Create strings.py

---
+++ 01-basics/strings.py
@@ -0,0 +1,23 @@
+# Strings sind Zeichenketten bestehend aus mehreren Zeichen
+print("Hallo Welt")
+
+# Speichern in einer Variable -> Angeben in Single oder Double Quotes
+a = "Test String!"
+b = "Hallo"
+print(a + " " + b)
+
+# Zahlen anhängen und Zahl in String umwandeln -> str(Zahl)
+print(a + " " + str(5))
+
+# oder
+print(a + " " + "5")
+
+
+# Zahl
+a = 4
+
+# String
+b = "Hallo"
+
+# Zahl als String
+c = str(5)
