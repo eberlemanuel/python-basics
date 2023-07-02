53c8edcd7fd6fa5ae3ef4a1adb88459d693160f6
Manuel Eberle <manueleberle9@gmail.com>
2023-07-02 14:06:05 +0200

    Create quiz.py

---
+++ 01-basics/quiz.py
@@ -0,0 +1,14 @@
+# Eine Zahl oder String
+v = 1
+
+# Eine Zahl oder String
+v = "1"
+
+# Eine Zahl oder String
+v = str(6)
+
+# Welche Zeile erzeugt keinen Fehler?
+print("Dein Guthaben beträgt " & str(50) & " Cent.") 
+print("Du hast nur noch " + str(10) + "€ auf dem Konto.") 
+print("Du hast " + 50000 + "€ gewonnen.") 
+print(String(Du hast Schulden.))
